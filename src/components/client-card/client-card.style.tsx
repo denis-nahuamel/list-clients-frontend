4cf7c4ed09b5b420ff384b4f236a59943cb83d31
Denis Nahuamel <nahuamel.denis@gmail.com>
2022-08-26 11:58:34 -0500

    add client-card styles

--- src/components/client-card/client-card.style.tsx
+++ src/components/client-card/client-card.style.tsx
@@ -1,16 +1,47 @@
 import styled from "@emotion/styled";
-import { fDirRow, fSpaceBet } from "../../styles/general";
-
+import { fAlignCenter, fDirCol, fDirRow, flexContainer, fSpaceBet } from "../../styles/general";
+import { css } from '@emotion/react'
 export const CardContainer = styled('div')`
-    width: 100%;
-    background-color: white;
+    ${flexContainer};
+    ${fDirCol};
+    gap: 0.8rem;
+    width: 15rem;
+    background-color: #F5F4F9;
     border-radius: 10px;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-    padding: 0.5rem 2rem;
+    padding: 0.8rem 1.5rem;
     margin: 0.5rem;
 `;
+const basicContainer = css`
+    ${flexContainer};
+    ${fAlignCenter};
+    gap: 0.2rem;
+`
 export const RowContainer = styled('div')`
-${fDirRow};
-${fSpaceBet};
-gap: 1rem
+    ${flexContainer};
+    ${fDirRow};
+    ${fSpaceBet};
+    ${fAlignCenter}
+`
+export const ElementContainer = styled('div')`
+    ${basicContainer};
+`
+export const MoneyContainer = styled('div')`
+    ${basicContainer};
+    padding: 0.2rem 1rem;
+    border-radius: 0.5rem;
+    background-color: #D9D9D9;
+`
+const BasicButton = css`
+    width: 1rem;
+    height: 1rem;
+    border-radius: 1rem;
+`
+export const GreenButton = styled('div')`
+    ${BasicButton};
+    background-color: green;
+`
+export const RedButton = styled('div')`
+    ${BasicButton};
+    background-color: red;
 `
--- src/components/client-card/client-card.tsx
+++ src/components/client-card/client-card.tsx
@@ -1,25 +1,45 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
-import { IClient } from "../../models/client.interface"
-import { fDirRow } from "../../styles/general"
-import { CardContainer, RowContainer } from "./client-card.style"
+import world from '../../assets/icons/world.png'
+import money from '../../assets/icons/money.png'
+import calendar from '../../assets/icons/calendar.png'
+import { EEstado, IClient } from "../../models/client.interface"
+import { CardContainer, ElementContainer, GreenButton, MoneyContainer, RedButton, RowContainer } from "./client-card.style"
 
-export const ClientCard = (client:IClient) =>{
+export const ClientCard = (client: IClient) => {
     return (
         <CardContainer>
-            <div>{client.nombres}</div>
             <RowContainer>
-                <div>fecha: {client.fecha}</div>
-                <div>monto: {client.monto}</div>
-            </RowContainer>
-            <RowContainer>
-                <div>pais: {client.pais}</div>
-                <div>tipo: {client.tipo}</div>
+                <ElementContainer>
+                    <img src={world} alt="world" />
+                    {client.pais}
+                </ElementContainer>
+                <ElementContainer>
+                    <img src={calendar} alt="calendar" />
+                    {client.fecha}
+                </ElementContainer>
             </RowContainer>
             <div>
-                {client.estado}
+                <strong style={{ color: "#00462C" }}>Nombres:</strong>
+                <div>{client.nombres}</div>
             </div>
+            <RowContainer>
+                <MoneyContainer>
+                    <img src={money} alt="money" style={{ width: '1.5rem' }} />
+                    {client.monto}
+                </MoneyContainer>
+                <div>
+                    <strong>Tipo:</strong>
+                    {client.tipo}
+                </div>
+            </RowContainer>
+            <RowContainer>
+                <ElementContainer>
+                    {client.estado === 1 ? <GreenButton /> : <RedButton />}
+                    {EEstado[client.estado]}
+                </ElementContainer>
+            </RowContainer>
         </CardContainer>
     )
 }
